1ffb960d74300f08783fbfc09da841f75d18a023
Israel Manzo <israman814@gmail.com>
2017-05-19 21:30:00 -0400

    Adding gradient animation background

---
+++ animationsGif/animationsGif/DoceViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DoceViewController.swift
+//  animationsGif
+//
+//  Created by Israel Manzo on 5/19/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class DoceViewController: UIViewController {
+    
+    @IBOutlet weak var gradientView: UIImageView!
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        animateBck()
+    }
+    
+    func animateBck(){
+        UIView.animate(withDuration: 15, delay: 0, options: [.autoreverse, .curveLinear, .repeat], animations: {
+            
+            let x = -(self.gradientView.frame.width - self.view.frame.width)
+            self.gradientView.transform = CGAffineTransform(translationX: x, y: 0)
+        })
+    }
+    
+}
