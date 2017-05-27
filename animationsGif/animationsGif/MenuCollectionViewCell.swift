d5e0fbeb7e867985e369c46a1a366ce361cace07
Israel Manzo <israman814@gmail.com>
2017-05-27 18:30:54 -0400

    Updating VC

---
+++ animationsGif/animationsGif/MenuCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MenuCollectionViewCell.swift
+//  animationsGif
+//
+//  Created by Israel Manzo on 5/27/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var menuLbl: UILabel!
+}
